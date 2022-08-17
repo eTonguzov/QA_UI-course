93c8d20eb6835cfffa9d47eaa16d2c9cf4de51cc
eTonguzov <etonguzov@gmail.com>
2022-08-17 15:48:34 +0300

    Lesson-05

---
+++ Lesson-05/src/fibonacci_numbers/Fibonacci.java
@@ -0,0 +1,27 @@
+package fibonacci_numbers;
+//Description:
+//Образуется посл-ть чисел Фибонначи (1, 1, 2, 3, 5, 8...)
+//Дано натуральное число n.
+//1. Найти k-й член последовательности Фибонначи
+//2. Получить первые n членов последовательноти Фибоннначи
+//3. Верно ли, что сумма превых n чисел посл-ти Фиббоначи есть четное число?
+
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Fibonacci {
+    public static void main(String[] args) throws Exception {
+//        int count = 1;
+//        int n = 10;
+//        ArrayList<Integer> list = new ArrayList<Integer>();//создаем неограниченный массив
+//        for(int i; i<=n; i++){
+
+       // BigInteger[] arr = new BigInteger[n];
+
+        }
+
+    }
+
+
+
